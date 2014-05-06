c284636c364696f8bc091747654d261acc4f50ed
Alexandre <alexandre.bacco@gmail.com>
2014-05-06 11:17:39 +0800

    Replace payment pre_state_change event

--- Controller/InventoryUnitController.php
+++ Controller/InventoryUnitController.php
@@ -18,23 +18,21 @@ use Symfony\Component\HttpFoundation\Request;
 
 class InventoryUnitController extends ResourceController
 {
-    public function updateStateAction(Request $request, $transitionName)
+    public function updateStateAction(Request $request, $transition)
     {
         $unit = $this->findOr404($request);
 
-        $this->get('event_dispatcher')->dispatch(
-            SyliusInventoryUnitEvents::PRE_STATE_CHANGE,
-            new GenericEvent($unit, array('transition' => $transitionName))
-        );
+        $stateMachine = $this->get('finite.factory')->get($unit, InventoryUnitTransitions::GRAPH);
+        if (!$stateMachine->can($transition)) {
+            $this->flashHelper->setFlash('error', 'sylius.inventory_unit.transition_fail');
+            return $this->redirectHandler->redirectToReferer();
+        }
+
+        $stateMachine->apply($transition);
 
         $this->domainManager->update($unit);
 
-        $this->get('event_dispatcher')->dispatch(
-            SyliusInventoryUnitEvents::POST_STATE_CHANGE,
-            new GenericEvent($unit, array('transition' => $transitionName))
-        );
-
-        $this->flashHelper->setFlash('success', 'sylius.inventory_unit.'.$transitionName);
+        $this->flashHelper->setFlash('success', 'sylius.inventory_unit.'.$transition);
 
         return $this->redirectHandler->redirectToReferer();
     }
